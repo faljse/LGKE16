6e8bfca5b85578a80e480e0e91d7247f8dfe5b83
Martin Kunz <martin.michael.kunz@gmail.com>
2016-04-15 11:41:14 +0200

    Restore functionality of CSVReadTool

--- restservice/src/main/java/org/omilab/robot/world/CSVReadTool.java
+++ restservice/src/main/java/org/omilab/robot/world/CSVReadTool.java
@@ -4,25 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class CSVReadTool {
+public class CSVReadTool implements AutoCloseable{
 	private BufferedReader br;
 	private String[] line;
 	private int elementnumber;
 
-	CSVReadTool(String csvFile){
+	CSVReadTool(String csvFile) throws IOException {
 		br = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/"+csvFile)));
-	}
-
-	public boolean closeFile(String csvFile) {
-		if (br != null) {
-			try {
-				br.close();
-				return true;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		return false;
+		nextLine();
 	}
 	
 	public String readLineElement() throws IOException {
@@ -36,22 +25,25 @@ public class CSVReadTool {
 	}
 	
 	public boolean nextLine() throws IOException {
-		String cvsSplitBy = ";";
+		final String csvSplitBy = ";";
 		String wholeLine;
 
-			if ((wholeLine = br.readLine()) != null) {
-				line = wholeLine.split(cvsSplitBy);
-				elementnumber = 0;
-				return true;
-			}
-			else
-				line = null;
-				elementnumber = -1;
-			
-		System.out.println("");
-		System.out.println("eof");
-		System.out.println("");
+		if ((wholeLine = br.readLine()) != null) {
+			line = wholeLine.split(csvSplitBy);
+			elementnumber = 0;
+			return true;
+		}
+		else
+			line = null;
+			elementnumber = -1;
 		return false;
 	}
- }
+
+	@Override
+	public void close() throws Exception {
+		try{
+			br.close();
+		} catch (IOException e){}
+	}
+}
 
